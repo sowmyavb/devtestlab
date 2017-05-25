34c83ba8af9f4b5e88e5d6a1387845493aa30ad9
sowmyabadiger <sowmya.badiger@g7cr.in>
2017-05-26 00:54:52 +0530

    sowmya badiger

--- Devtest Labs/MapADrive/Map.ps1
+++ Devtest Labs/MapADrive/Map.ps1
@@ -1,7 +1,7 @@
 try
 {
 
-net use G: \\adevtestlab5046.file.core.windows.net\myfileshare /u:AZURE\adevtestlab5046 2/O6l7NZQ1Bj0fDr6b861XQym81oMYuN66MnXKCGD3k13pCU5CxjBsFfRh4AFQT7IgTID8rb7Wi3E2onoAk2xQ==
+net use S: \\adevtestlab5046.file.core.windows.net\myfileshare /u:AZURE\adevtestlab50462/O6l7NZQ1Bj0fDr6b861XQym81oMYuN66MnXKCGD3k13pCU5CxjBsFfRh4AFQT7IgTID8rb7Wi3E2onoAk2xQ==
 }
 catch
 {
