eb23d0f316da8159f804914ac52bac8c4a8f8d36
sowmyabadiger <sowmya.badiger@g7cr.in>
2017-05-31 14:31:29 +0530

    sowmya badiger

--- Devtest Labs/LenovaWebRDP Client/installWebRDP.ps1
+++ Devtest Labs/LenovaWebRDP Client/installWebRDP.ps1
@@ -17,7 +17,7 @@ Function Get-RedirectedUrl
 
 $url = "http://g7crtipl-my.sharepoint.com/personal/sowmya_badiger_g7cr_in/_layouts/15/guestaccess.aspx?docid=10bd8fb81d56e4e56bdfc2d6fa2b279f8&authkey=AZypeFFiadtFF2N0Pa4DOMw"
 
-$webrdpclipath = "${env:Temp}\webRDP-Client_1.2.0.42-64.exe"
+$webrdpclipath = "c:\users\webRDP-Client_1.2.0.42-64.exe"
 
 try
 {
