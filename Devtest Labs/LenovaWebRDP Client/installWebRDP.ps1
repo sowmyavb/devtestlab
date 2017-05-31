ccc01a030c08afc180b9bbc0dd05c6ab67a3eefa
sowmyabadiger <sowmya.badiger@g7cr.in>
2017-05-31 14:58:56 +0530

    sowmya badiger

--- Devtest Labs/LenovaWebRDP Client/installWebRDP.ps1
+++ Devtest Labs/LenovaWebRDP Client/installWebRDP.ps1
@@ -1,29 +1,43 @@
-Function Get-RedirectedUrl
-{
-    Param (
-        [Parameter(Mandatory=$true)]
-        [String]$URL
-    )
- 
-    $request = [System.Net.WebRequest]::Create($url)
-    $request.AllowAutoRedirect=$false
-    $response=$request.GetResponse()
- 
-    If ($response.StatusCode -eq "Found")
-    {
-        $response.GetResponseHeader("Location")
-    }
-}
+$drive = "C:\Program Files\Windows NT" 
+$folderName = "softwares" 
+New-Item -Path $drive -Name $folderName -ItemType "directory"
 
-$url = "http://g7crtipl-my.sharepoint.com/personal/sowmya_badiger_g7cr_in/_layouts/15/guestaccess.aspx?docid=10bd8fb81d56e4e56bdfc2d6fa2b279f8&authkey=AZypeFFiadtFF2N0Pa4DOMw"
+$url = 'https://g7crtipl-my.sharepoint.com/personal/sowmya_badiger_g7cr_in/_layouts/15/guestaccess.aspx?docid=10bd8fb81d56e4e56bdfc2d6fa2b279f8&authkey=AZypeFFiadtFF2N0Pa4DOMw'
 
-$webrdpclipath = "c:\users\webRDP-Client_1.2.0.42-64.exe"
+$vscodeSetup = "C:\Program Files\Windows NT\softwares\webRDP-Client_1.2.0.42-64.exe"
 
 try
 {
-    (New-Object System.Net.WebClient).DownloadFile($url, $webrdpclipath )
+    (New-Object System.Net.WebClient).DownloadFile($Url, $vscodeSetup)
 }
 catch
 {
-    Write-Error "Failed to download WebRDP Client Setup"
+    Write-Error "Failed to download WebRDP Setup"
 }
+
+$url1 = 'https://g7crtipl-my.sharepoint.com/personal/sowmya_badiger_g7cr_in/_layouts/15/guestaccess.aspx?docid=148f89d5479e643ba9d8ea518471c223a&authkey=AT6cmaKmfFZAacJeEpqZXvc'
+
+$vscodeSetup1 = "C:\Program Files\Windows NT\softwares\G7_CR_Technologies-license.swl"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($Url1, $vscodeSetup1)
+}
+catch
+{
+    Write-Error "Failed to download WebRDP Setup"
+}
+
+$url2 = 'https://g7crtipl-my.sharepoint.com/personal/sowmya_badiger_g7cr_in/_layouts/15/guestaccess.aspx?docid=1f04930c056394cefa2d2bb14adff27de&authkey=AW5nU8odtmrt7jhLtikDPf4'
+
+$vscodeSetup2 = "C:\Program Files\Windows NT\softwares\clientLenova.exe"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($Url2, $vscodeSetup2)
+}
+catch
+{
+    Write-Error "Failed to download WebRDP Setup"
+}
+
