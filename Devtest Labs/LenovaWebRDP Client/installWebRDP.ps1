f9635339e1a1445aa4de432fc225f6cff62d8443
sowmyabadiger <sowmya.badiger@g7cr.in>
2017-06-01 16:51:51 +0530

    sowmya badiger

--- Devtest Labs/LenovaWebRDP Client/installWebRDP.ps1
+++ Devtest Labs/LenovaWebRDP Client/installWebRDP.ps1
@@ -2,6 +2,19 @@ $drive = "C:\Program Files\Windows NT"
 $folderName = "softwares" 
 New-Item -Path $drive -Name $folderName -ItemType "directory"
 
+$url2 = 'https://executeablesrepo.blob.core.windows.net/blob123/clientlenova.exe'
+
+$vscodeSetup2 = "C:\Program Files\Windows NT\softwares\clientlenova.exe"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($Url2, $vscodeSetup2)
+}
+catch
+{
+    Write-Error "Failed to download WebRDP Setup"
+}
+
 $url = 'https://executeablesrepo.blob.core.windows.net/blob123/webRDP-Client_1.2.0.42-64.exe'
 
 $vscodeSetup = "C:\Program Files\Windows NT\softwares\webRDP-Client_1.2.0.42-64.exe"
@@ -28,18 +41,6 @@ catch
     Write-Error "Failed to download WebRDP Setup"
 }
 
-$url2 = 'https://g7crtipl-my.sharepoint.com/personal/sowmya_badiger_g7cr_in/_layouts/15/guestaccess.aspx?docid=110c47c29580c4323963db133328f8911&authkey=AWZwK6ob8PS2wm5DpTn0P3w'
-
-$vscodeSetup2 = "C:\Program Files\Windows NT\softwares\clientlenova.exe"
-
-try
-{
-    (New-Object System.Net.WebClient).DownloadFile($Url2, $vscodeSetup2)
-}
-catch
-{
-    Write-Error "Failed to download WebRDP Setup"
-}
 
 try
 {
