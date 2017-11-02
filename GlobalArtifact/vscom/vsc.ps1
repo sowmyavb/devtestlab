20d58d00a6a6e196f163b6a23539c0f6e00b69a5
sowmyavb <selvi.m@g7cr.in>
2017-11-02 17:15:15 +0530

    sowmya badiger

---
+++ GlobalArtifact/vscom/vsc.ps1
@@ -0,0 +1,30 @@
+﻿Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+   
+}
+$url = 'https://cheekandchongsupinthesmoke-my.sharepoint.com/personal/bulletformyvalentine_cheekandchongsupinthesmoke_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=1197a59ab31594530b932e6fa1b79ea0e&authkey=AQJTZdnA_Y4J7gnCeW6wtsM'
+$codeSetupUrl = "$PSScriptroot\10meg.exe"
+$vscodeSetup = "${env:Temp}\vs_community.exe"
+
+
+try
+{
+    Invoke-WebRequest -Uri $Url -OutFile $vscodeSetup;
+}
+catch
+{
+    Write-Error "Failed to download VS 2017";
+}
+try
+{
+    Start-Process -FilePath $vscodeSetup -ArgumentList "--add Microsoft.VisualStudio.Workload.NetCoreTools;includeRecommended --passive";
+}
+catch
+{
+    Write-Error "Failed to install VS 2017";
+}
---
+++ GlobalArtifact/vsprof/vprf.ps1
@@ -0,0 +1,28 @@
+﻿Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )  
+}
+$url = 'https://cheekandchongsupinthesmoke-my.sharepoint.com/personal/bulletformyvalentine_cheekandchongsupinthesmoke_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=119fa9020ec3b4595ba1062a2eaf30ab1&authkey=ATPQzQ23crlLEitcIDceGHE'
+$codeSetupUrl = "$PSScriptroot\10meg.exe"
+$vscodeSetup = "${env:Temp}\vs_community.exe"
+
+
+try
+{
+    Invoke-WebRequest -Uri $Url -OutFile $vscodeSetup;
+}
+catch
+{
+    Write-Error "Failed to download VS 2017";
+}
+try
+{
+    Start-Process -FilePath $vscodeSetup -ArgumentList "--add Microsoft.VisualStudio.Workload.NetCoreTools;includeRecommended --passive";
+}
+catch
+{
+    Write-Error "Failed to install VS 2017";
+}
